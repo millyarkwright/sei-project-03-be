66423c79b3ad4e56103fb090fb4e982c506bf8c7
Milly Arkwright <millyarkwright@Millys-MacBook-Air.local>
2022-08-18 12:48:09 +0100

    Folders added, Logger added, Cors installed

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 import express from 'express'
+import cors from 'cors'
+import logger from './middleware/logger.js'
 
 const PORT = 4000
 
-// ! Define Express App
+// ? Define Express App
 const app = express()
 
-app.get('/', (req, res, next) => {
+// ? Allow requests from anywhere
+app.use(cors())
+
+// ? Convert request into valid JSON object
+app.use(express.json())
+
+app.use(logger)
+
+app.get('/', (req, res) => {
   return res.status(200).send('Api is running')
+})
+
+
+// ! Catching anything that falls through to this point
+app.use((req, res) => {
+  return res.status(404).send('Required endpoint not found')
 })
 
 // ! Start Server
